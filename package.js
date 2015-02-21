7abf7e9dfdee54fccccfb85d5400468a2ec85578
renatosantana <renatosilva.dcc@gmail.com>
2015-02-21 05:49:16 +0000

    alteração de configarações no pacote

--- package.js
+++ package.js
@@ -2,17 +2,18 @@ Package.describe({
   name: 'horhekenu:box2dweb',
   version: '2.1.3',
   // Brief, one-line summary of the package.
-  summary: 'The Box2D physics engine was developed by Erin Catto (visit http://www.gphysics.com for further information). Release package 2.1.a.3 ',
+  summary: 'Engine was developed by Erin Catto (visit http://www.gphysics.com for further information)',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/RenatoSantana/Box2dWeb.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: null
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.1');
-  api.addFiles('horhekenu:box2dweb.js');
+  api.use("Box2D", "client");
+  api.versionsFrom('1.0.3.2');
+  api.addFiles('horhekenu:box2dweb.js',"client");
 });
 
 Package.onTest(function(api) {
